5eb6cb248a9583cbbdc421bbdfa149aecfd5abbf
Folke Will <folko@solhost.org>
2018-04-04 11:11:15 +0200

    Limit FPS since data is moot after reloading plugins

--- src/avitab/apps/HeaderApp.cpp
+++ src/avitab/apps/HeaderApp.cpp
@@ -73,7 +73,7 @@ float HeaderApp::getAverageFPS() {
     size_t count = 0;
 
     std::for_each(std::begin(fpsRingBuffer), std::end(fpsRingBuffer), [&fpsSum, &count] (float f) {
-        if (f > 0) {
+        if (f > 0 && f < 1000) {
             fpsSum += f;
             count++;
         }
