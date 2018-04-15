2b46bd43db6cbe5af6cdebaee958f446c42eed2d
Folke Will <folko@solhost.org>
2018-04-15 22:31:10 +0200

    Add checkbox to GUI

---
+++ src/gui_toolkit/widgets/Checkbox.cpp
@@ -0,0 +1,40 @@
+/*
+ *   AviTab - Aviator's Virtual Tablet
+ *   Copyright (C) 2018 Folke Will <folko@solhost.org>
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU Affero General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Affero General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Affero General Public License
+ *   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+#include "Checkbox.h"
+
+namespace avitab {
+
+Checkbox::Checkbox(WidgetPtr parent, const std::string& caption):
+    Widget(parent)
+{
+    lv_obj_t *cb = lv_cb_create(parentObj(), nullptr);
+
+    lv_cb_set_text(cb, caption.c_str());
+
+    setObj(cb);
+}
+
+void Checkbox::setChecked(bool check) {
+    lv_cb_set_checked(obj(), check);
+}
+
+bool Checkbox::isChecked() {
+    return lv_cb_is_checked(obj());
+}
+
+} /* namespace avitab */
---
+++ src/gui_toolkit/widgets/Checkbox.h
@@ -0,0 +1,34 @@
+/*
+ *   AviTab - Aviator's Virtual Tablet
+ *   Copyright (C) 2018 Folke Will <folko@solhost.org>
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU Affero General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Affero General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Affero General Public License
+ *   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+#ifndef SRC_GUI_TOOLKIT_WIDGETS_CHECKBOX_H_
+#define SRC_GUI_TOOLKIT_WIDGETS_CHECKBOX_H_
+
+#include "Widget.h"
+
+namespace avitab {
+
+class Checkbox: public Widget {
+public:
+    Checkbox(WidgetPtr parent, const std::string &caption);
+    void setChecked(bool check);
+    bool isChecked();
+};
+
+} /* namespace avitab */
+
+#endif /* SRC_GUI_TOOLKIT_WIDGETS_CHECKBOX_H_ */
