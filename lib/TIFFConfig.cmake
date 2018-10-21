0e2e9b51bfb1c1a7e48f7bfb12e31cd42cca61ce
Folke Will <folko@solhost.org>
2018-10-21 16:55:04 +0200

    Add lm to geotiff

--- lib/TIFFConfig.cmake
+++ lib/TIFFConfig.cmake
@@ -1,3 +1,3 @@
 set(TIFF_LIBRARY "${CMAKE_CURRENT_LIST_DIR}/libtiff/libtiff/.libs/libtiff.a")
 set(TIFF_INCLUDE_DIR "${CMAKE_CURRENT_LIST_DIR}/libtiff/libtiff")
-set(TIFF_LIBRARIES ${TIFF_LIBRARY} "${CMAKE_CURRENT_LIST_DIR}/mupdf/build/release/libmupdf-third.a")
+set(TIFF_LIBRARIES ${TIFF_LIBRARY} "${CMAKE_CURRENT_LIST_DIR}/mupdf/build/release/libmupdf-third.a" m)
