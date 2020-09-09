ee7d480a0bede06a3f7f222f3b1356f0f0a575cb
Folke Will <folko@solhost.org>
2020-09-09 18:00:17 +0200

    Fix Linux compilation

--- src/environment/Settings.cpp
+++ src/environment/Settings.cpp
@@ -16,6 +16,7 @@
  *   along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
+#include <iomanip>
 #include <fstream>
 #include "src/platform/Platform.h"
 #include "Settings.h"
